5c0823c62d44559436ed91269d94a63ad93e4bf7
kkiyama117 <k.kiyama117@gmail.com>
2019-12-02 13:59:06 +0900

    Add Licence

--- revol_base/settings.py
+++ revol_base/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'Please_replace_secret_key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-JP'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Tokyo'
 
 USE_I18N = True
 
