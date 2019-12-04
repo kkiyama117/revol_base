0b1419dc02776a79f80a51ac72d3e7135f4a0d1c
kkiyama117 <k.kiyama117@gmail.com>
2019-12-04 20:29:19 +0900

    Add group

--- society/models.py
+++ society/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Society(models.Model):
     name = models.CharField(max_length=30, blank=False)
-    leader = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    # leader = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
